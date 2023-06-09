6b9c045dc3440241ba122c8ef45645ce2bffb928
Vinícius Melo <vinimelo@riseup.net>
2023-06-09 19:46:16 -0300

    Inicia projeto com create-next-app

---
+++ README.md
@@ -0,0 +1,13 @@
+## cuidado-digital
+
+Para rodar o projeto, primeiro instale os pacotes necessários.
+
+```sh
+yarn install
+```
+
+Depois, execute o projeto em modo de desenvolvimento.
+
+```sh
+yarn dev
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Cuidado Digital</title>
+      </Head>
+      <main>
+        <h1>Cuidado Digital</h1>
+      </main>
+    </>
+  );
+}
