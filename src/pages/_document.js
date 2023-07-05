e33335e46fcfaa133768ab62a06d65001c1fae85
Vinícius Melo <vinimelo@riseup.net>
2023-07-05 12:01:49 -0300

    Ajusta analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Link from 'next/link';
-import { Analytics } from '@vercel/analytics/react';
 
 export default function Document() {
   return (
@@ -15,7 +14,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   )
